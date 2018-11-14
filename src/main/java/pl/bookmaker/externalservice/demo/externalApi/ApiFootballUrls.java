bdd7e1b7e593bae38afff5d8eeff50c729ae866b
cayman25 <mariusz_kajman@wp.pl>
2018-11-14 23:29:14 +0100

    Update src/main/java/pl/bookmaker/externalservice/demo/externalApi/ApiFootballUrls.java

--- src/main/java/pl/bookmaker/externalservice/demo/externalApi/ApiFootballUrls.java
+++ src/main/java/pl/bookmaker/externalservice/demo/externalApi/ApiFootballUrls.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import pl.bookmaker.externalservice.demo.converters.DateParser;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Component
@@ -16,38 +13,24 @@ class ApiFootballUrls {
     @Value("${api.url}")
     String url;
 
-    /*List<String> createListUrl() {
-        List<String> urls = new ArrayList<String>();
-
-        HashMap<Integer,String> leagues = createLeagueObjects();
-        leagues.forEach( (K,V) -> {
-            urls.add(url + K + "/matches?" +
-                    "dateFrom="+ DateParser.getTodayWithAddOrSubstractionOfDay(-10) +
-                    "&dateTo=" + DateParser.getTodayWithAddOrSubstractionOfDay(14));
-        });
-        return urls;
-    }*/
-
     List<String> createListUrl() {
         HashMap<Integer,String> leagues = createLeagueObjects();
 
         return leagues.entrySet().stream().map(p ->
                         url + p.getKey() + "/matches?" +
-                            "dateFrom="+ DateParser.getTodayWithAddOrSubstractionOfDay(-10) +
+                            "dateFrom="+ DateParser.getTodayWithAddOrSubstractionOfDay(-1) +
                             "&dateTo=" + DateParser.getTodayWithAddOrSubstractionOfDay(14))
                         .collect(Collectors.toList());
     }
 
     LinkedHashMap<Integer, String> createLeagueObjects() {
-        LinkedHashMap<Integer, String> listOfAvailableLeagues = new LinkedHashMap<Integer, String>();
-        listOfAvailableLeagues.put(2021, "England: Premier League");
-        listOfAvailableLeagues.put(2016, "England: Championship");
-        listOfAvailableLeagues.put(2015, "France: Ligue 1");
-        listOfAvailableLeagues.put(2002, "Germany: Bundesliga");
-        listOfAvailableLeagues.put(2019, "Italy: Serie A");
-        listOfAvailableLeagues.put(2014, "Spain: Primera Division");
-        listOfAvailableLeagues.put(2001, "Europe: UEFA Champions League");
-        return listOfAvailableLeagues;
+        return new LinkedHashMap<Integer, String>(Map.ofEntries
+                (Map.entry(2021, "England: Premier League"),
+                (Map.entry(2016, "England: Championship")),
+                (Map.entry(2015, "France: Ligue 1")),
+                (Map.entry(2002, "Germany: Bundesliga")),
+                (Map.entry(2019, "Italy: Serie A")),
+                (Map.entry(2014, "Spain: Primera Division")),
+                (Map.entry(2001, "Europe: UEFA Champions League"))));
     }
-
 }
